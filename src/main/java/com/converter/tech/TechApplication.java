91df4126b81c691f8d2eabed9570af9a94470b3a
her0ku <drdrink41@gmail.com>
2021-03-29 20:33:28 +0300

    full

--- src/main/java/com/converter/tech/TechApplication.java
+++ src/main/java/com/converter/tech/TechApplication.java
@@ -14,27 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public abstract class TechApplication{
-/*
-    @Autowired
-    RateService rateService;
-
-    @Autowired
-    CurrencyService currencyService;
-*/
     public static void main(String[] args) {
         SpringApplication.run(TechApplication.class, args);
     }
-/*
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        XMLData xmlData = XMLService.XMLDataParse();
-        for(Rate r : xmlData.getRateList())
-        {
-            rateService.saveAllRates(r);
-        }
-        for(Currency c : xmlData.getCurrencyList())
-        {
-            currencyService.addAllCurrency(c);
-        }
-    }*/
 }
